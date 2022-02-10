09fdb06602bf1f034d22212b8a9065b131ccfced
hasibmiraz <hasibmiraz09@gmail.com>
2022-02-10 20:17:34 +0600

    Full completed

--- js/banking.js
+++ js/banking.js
@@ -2,11 +2,41 @@
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
-    let depositAmount = parseInt(
+    const depositAmount = parseInt(
       document.getElementById("deposit-input").value
     );
-    const totalDeposit = parseInt(
-      document.getElementById("deposit-amount").innerText
+    const totalDeposit =
+      parseInt(document.getElementById("deposit-amount").innerText) +
+      depositAmount;
+
+    document.getElementById("deposit-amount").innerText = totalDeposit;
+
+    const totalAmount = parseInt(
+      document.getElementById("total-amount").innerText
     );
-    console.log(totalDeposit);
+    document.getElementById("total-amount").innerText =
+      totalAmount + totalDeposit;
+    document.getElementById("deposit-input").value = "";
+  });
+
+// Handle withdraw button
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawAmount = parseInt(
+      document.getElementById("withdraw-input").value
+    );
+
+    const totalWithdraw =
+      parseInt(document.getElementById("withdraw-amount").innerText) +
+      withdrawAmount;
+    document.getElementById("withdraw-amount").innerText = totalWithdraw;
+
+    const totalAmount = parseInt(
+      document.getElementById("total-amount").innerText
+    );
+    document.getElementById("total-amount").innerText =
+      totalAmount - totalWithdraw;
+
+    document.getElementById("withdraw-input").value = "";
   });
